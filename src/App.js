ef2355620e752902f1c2a56b00d9c62814a8a316
andreferi3 <andreferi135@gmail.com>
2019-08-20 23:22:34 +0700

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom' 
 import Pallete from './Pallete'
 import seedColors from './seedColors'
 import { generatePallete } from './ColorHelper'
 
 function App() {
-  console.log(generatePallete(seedColors[4]))
   return (
-    <div>
-      <Pallete pallette={generatePallete(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Pallete List Goes Here!</h1>} />
+      <Route exact path='/pallete/:id' render={() => <h1>Individual Pallete</h1>} />
+      {/* <div>
+        <Pallete pallette={generatePallete(seedColors[4])} />
+      </div> */}
+    </Switch>
   );
 }
 
